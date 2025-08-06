5e457f2b611ea6e534aa715089342990faf0d27d
Gael <gaelgilbello27@gmail.com>
2025-08-05 17:56:04 -0700

    updated projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,88 +1,90 @@
-import one from '../assets/svg/projects/one.svg'
-import two from '../assets/svg/projects/two.svg'
-import three from '../assets/svg/projects/three.svg'
-import four from '../assets/svg/projects/four.svg'
-import five from '../assets/svg/projects/five.svg'
-import six from '../assets/svg/projects/six.svg'
-import seven from '../assets/svg/projects/seven.svg'
-import eight from '../assets/svg/projects/eight.svg'
-
+import one from "../assets/svg/projects/one.svg";
+import two from "../assets/svg/projects/two.svg";
+import three from "../assets/svg/projects/three.svg";
+import four from "../assets/svg/projects/four.svg";
+import five from "../assets/svg/projects/five.svg";
+import six from "../assets/svg/projects/six.svg";
+import seven from "../assets/svg/projects/seven.svg";
+import eight from "../assets/svg/projects/eight.svg";
 
 export const projectsData = [
-    {
-        id: 1,
-        projectName: 'Algorithm Visualiser',
-        projectDesc: 'A project I created using react to visualize sorting algorithms and pathfinding algorithms. ',
-        tags: ['Algorithms', 'Sorting', 'Graphs'],
-        code: 'https://github.com/GaelGil/algorithm-visualizer',
-        demo: 'https://gaelgil.github.io/algorithm-visualizer/',
-        image: one
-    },
-    {
-        id: 2,
-        projectName: 'Web Scraper',
-        projectDesc: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
-        tags: ['Selenium', 'Python'],
-        code: 'https://github.com/GaelGil/web-scraper',
-        demo: '',
-        image: two
-    },
-    {
-        id: 3,
-        projectName: 'Spotify Playlist Maker',
-        projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
-        tags: ['Python', 'Spotify', 'API'],
-        code: 'https://github.com/GaelGil/spotify-playlist-maker',
-        demo: '',
-        image: three
-    },
-    {
-        id: 4,
-        projectName: 'Neural Network From Scratch',
-        projectDesc: 'This is a notebook implements a neural network from scratch using numpy',
-        tags: ['Math', 'Deep Learning', 'MNIST'],
-        code: 'https://github.com/GaelGil/notebooks/blob/master/back-propagation/back_propagation.ipynb',
-        demo: '',
-        image: four
-    },
-    {
-        id: 5,
-        projectName: 'Probability Theory',
-        projectDesc: 'This is a notebook that goes over probability theory basiscs',
-        tags: ['Math', 'Probability Theory'],
-        code: 'https://github.com/GaelGil/notebooks/blob/master/probability_theory/probability_theory.ipynb',
-        demo: '',
-        image: five
-    },
-    {
-        id: 6,
-        projectName: 'Convolutional Neural Networks',
-        projectDesc: 'A basics on convolutional networks',
-        tags: ['Python', 'Deep Learning', 'PyTorch'],
-        code: 'https://github.com/GaelGil/notebooks/blob/master/convolutional_nueral_networks/cnns.ipynb',
-        demo: '',
-        image: six
-    },
-    {
-        id: 7,
-        projectName: 'Sentence Generator',
-        projectDesc: 'A simple sentence generator using markov chains',
-        tags: ['Python', 'Math', 'Markov Chains'],
-        code: 'https://github.com/GaelGil/sentence-generator',
-        demo: '',
-        image: seven
-    },
-    {
-        id: 8,
-        projectName: 'Car Pooling System',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: eight
-    },
-]
-
+  {
+    id: 1,
+    projectName: "Algorithm Visualiser",
+    projectDesc:
+      "A project I created using react typescript to visualize sorting algorithms and pathfinding algorithms. ",
+    tags: ["Algorithms", "Sorting", "Graphs"],
+    code: "https://github.com/GaelGil/algorithm-visualizer",
+    demo: "https://gaelgil.github.io/algorithm-visualizer/",
+    image: one,
+  },
+  {
+    id: 2,
+    projectName: "Web Scraper",
+    projectDesc:
+      "This project is a web scraper that scrapes data from a website and stores it in a csv file for analysis.",
+    tags: ["Selenium", "Python"],
+    code: "https://github.com/GaelGil/web-scraper",
+    demo: "",
+    image: two,
+  },
+  {
+    id: 3,
+    projectName: "Neural Network From Scratch",
+    projectDesc:
+      "This is a notebook implements a neural network from scratch using numpy",
+    tags: ["Math", "Deep Learning", "MNIST"],
+    code: "https://github.com/GaelGil/notebooks/blob/master/back-propagation/back_propagation.ipynb",
+    demo: "",
+    image: four,
+  },
+  {
+    id: 4,
+    projectName: "Spotify Playlist Maker",
+    projectDesc:
+      "A project that creates spotify playlists using other popular songs in other playlists",
+    tags: ["Python", "Spotify", "API"],
+    code: "https://github.com/GaelGil/spotify-playlist-maker",
+    demo: "",
+    image: three,
+  },
+  {
+    id: 5,
+    projectName: "Probability Theory",
+    projectDesc: "This is a notebook that goes over probability theory basiscs",
+    tags: ["Math", "Probability Theory"],
+    code: "https://github.com/GaelGil/notebooks/blob/master/probability_theory/probability_theory.ipynb",
+    demo: "",
+    image: five,
+  },
+  {
+    id: 6,
+    projectName: "Convolutional Neural Networks",
+    projectDesc: "A basics on convolutional networks",
+    tags: ["Python", "Deep Learning", "PyTorch"],
+    code: "https://github.com/GaelGil/notebooks/blob/master/convolutional_nueral_networks/cnns.ipynb",
+    demo: "",
+    image: six,
+  },
+  {
+    id: 7,
+    projectName: "Essay Writer AI Agent",
+    projectDesc: "An AI agent that plans and executes a plan to write an essay",
+    tags: ["Agent", "OpenAI", "Python"],
+    code: "https://github.com/GaelGil/essay_writing_ai_agent",
+    demo: "https://github.com/GaelGil/essay_writing_ai_agent",
+    image: eight,
+  },
+  {
+    id: 8,
+    projectName: "Sentence Generator",
+    projectDesc: "A simple sentence generator using markov chains",
+    tags: ["Python", "Math", "Markov Chains"],
+    code: "https://github.com/GaelGil/sentence-generator",
+    demo: "",
+    image: seven,
+  },
+];
 
 // Do not remove any fields.
 // Leave it blank instead as shown below
