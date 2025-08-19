f9e7dce0f36d3a3cbba02425ccbdc0e00f462284
Gael <gaelgilbello27@gmail.com>
2025-08-19 11:35:14 -0700

    updated the component name

--- src/components/Layout/Content.tsx
+++ src/components/Layout/Content.tsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 import Skills from "../Home/Skills";
 import Education from "../Home/Education";
 import Experince from "../Home/Experience";
-export default function Home() {
+export default function Content() {
   return (
     <main className="ml-[25%] h-screen px-8">
       <About />
