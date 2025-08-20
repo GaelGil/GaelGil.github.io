37e2f31722dd3297c97c34c892e3736a736914d8
Gael <gaelgilbello27@gmail.com>
2025-08-19 22:49:26 -0700

    updating footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,8 +1,8 @@
 import { NAME } from "../../data/const";
 export default function Footer() {
   return (
-    <section id="footer" className="md:ml-64  flex items-center">
-      <footer className="text-black py-6 text-center text-secondary-300">
+    <section id="footer">
+      <footer className="py-6 text-center text-secondary-300">
         Made with 💜 by {NAME}
       </footer>
     </section>
