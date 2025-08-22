ab7218f590179ced9b486a40c0de11623385f849
Gael <gaelgilbello27@gmail.com>
2025-08-22 00:40:05 -0700

    updating website

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,9 +1,12 @@
 import { NAME } from "../../data/const";
+import { FaHeart } from "react-icons/fa";
+
 export default function Footer() {
   return (
     <section id="footer">
       <footer className="py-6 text-center text-secondary-300">
-        Made with 💜 by {NAME}
+        Made with <FaHeart className="text-secondary-300 inline-block mr-1" />{" "}
+        by {NAME}
       </footer>
     </section>
   );
