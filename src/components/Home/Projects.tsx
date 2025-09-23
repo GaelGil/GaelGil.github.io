83b10dc762737c276f002cf61e1ec7bbab52ff07
Gael <gaelgilbello27@gmail.com>
2025-09-22 23:10:26 -0700

    fixed type bug

--- src/components/Home/Projects.tsx
+++ src/components/Home/Projects.tsx
@@ -6,20 +6,23 @@ export default function Projects() {
       {[...PROJECTS]
         .reverse()
         .slice(0, 4)
-        .map((p) => (
+        .map((project) => (
           <Link
-            key={p.id}
-            to={`/projects/${p.id}`}
+            key={project.id}
+            to="/projects/$id"
+            params={{ id: project.id.toString() }}
             className="rounded-md transition border-2 border-transparent hover:border-secondary-300 cursor-pointer flex"
           >
             <div className="flex-1">
-              <h5 className="px-3 py-3 text-lg font-semibold">{p.title}</h5>
+              <h5 className="px-3 py-3 text-lg font-semibold">
+                {project.title}
+              </h5>
               <p className="px-3 mt-2 text-sm leading-relaxed text-secondary-300">
-                {p.description}
+                {project.description}
               </p>
               {/* Tags */}
               <div className="px-3 mt-4 flex flex-wrap gap-2 ">
-                {p.tags?.map((t) => (
+                {project.tags?.map((t) => (
                   <span
                     key={t}
                     className="px-2 py-1 text-xs rounded-md border text-secondary-300 border-secondary-300 "
@@ -30,8 +33,8 @@ export default function Projects() {
               </div>
             </div>
             <img
-              src={p.img}
-              alt={p.title}
+              src={project.img}
+              alt={project.title}
               className="object-fit rounded-sm md:w-50 md:h-50 w-25 h-25 "
             />
           </Link>
