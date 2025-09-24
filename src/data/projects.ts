4cdd915601b92e487a0cdd8f10ad44d83236d881
Gael <gaelgilbello27@gmail.com>
2025-09-24 11:59:22 -0700

    updated project links

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -23,7 +23,7 @@ export const PROJECTS: Project[] = [
     content:
       " To generate sentences we use Markov chains. A Markov chain is a sequence of events where the probability of future events depend only on the current event. In our case the Markov chain is a sequence of words. Where the next word can be predicted based on the current word. I did this by creating a dictionary of all the words in a given text. Then for each word we get the following words and count them. We can see that some words are more common than others. For example car is more likely to be followed by drove rather than person or exploded. I applied softmax to this dictionary to accurately represent a Markov chain. This means all probabilities of each of the next words sum to 1. To learn more about the implementation check out the repo!",
     repo: "https://github.com/GaelGil/sentence-generator",
-    link: "https://gaelgil.github.io/sentence-generator/",
+    link: "https://sentence-generator.pages.dev/",
     tags: ["Flask", "Python", "Markov Chain", "Probability"],
     img: markov,
   },
@@ -83,7 +83,7 @@ export const PROJECTS: Project[] = [
     link: "https://github.com/GaelGil/algorithm-visualizer",
     content:
       "A project I created using react to visualize sorting algorithms and pathfinding algorithms. It uses react typescript and tailwind.",
-    repo: "https://gaelgil.github.io/algorithm-visualizer/",
+    repo: "https://algorithm-visualizer-a7v.pages.dev/",
     tags: ["React", "Typescript", "Algorithms", "Sorting", "Pathfinding"],
     img: algorithm_visualizer,
   },
